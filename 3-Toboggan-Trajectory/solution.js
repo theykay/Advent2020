7dfe37df10ae183af23e245dc690f6f6d4fccdcd
TheyKay <murraykyleb@gmail.com>
2020-12-03 22:12:15 -0700

    changed code to be flexible, solved part 2

--- 3-Toboggan-Trajectory/solution.js
+++ 3-Toboggan-Trajectory/solution.js
@@ -1,3 +1,4 @@
+const { codePointAt } = require("../2-Password-Philosophy/input");
 const input = require("./input");
 let forest = input.split(/\n/);
 
@@ -5,27 +6,45 @@ for (let i = 0; i < forest.length; i++) {
   forest[i] = forest[i].split("");
 }
 
-let collisions = 0;
-let position = 0;
+// function path2() {
+//   let collisions = 0;
+//   let position = 0;
+//   for (let branch = 0; branch < forest.length; branch++) {
+//     if (forest[branch][position] === "#") {
+//       collisions++;
+//     }
 
-for (let branch = 0; branch < forest.length; branch++) {
-  if (forest[branch][position] === "#") {
-    collisions++;
+//     if (position === 30) {
+//       position = 2
+//     } else if (position === 29) {
+//       position = 1
+//     } else if (position === 28) {
+//       position = 0
+//     } else {
+//       position = position + 3;
+//     }
+//   }
+//   console.log("path 2: ", collisions);
+// }
+
+function collider(over, down) {
+  let collisions = 0;
+  let position = 0;
+  for (let branch = 0; branch < forest.length; branch = branch + down) {
+    if (forest[branch][position] === "#") {
+      collisions++;
+    }
+    position = (position + over) % 31;
   }
-  // console.log("branch: ", branch);
-  // console.log("position: ", position);
-  // console.log("-------------------");
-
-
-  if (position === 30) {
-    position = 2
-  } else if (position === 29) {
-    position = 1
-  } else if (position === 28) {
-    position = 0
-  } else {
-    position = position + 3;
-  }
+  console.log(collisions);
+  return collisions;
 }
+const path1 = collider(1,1);
+const path2 = collider(3,1);
+const path3 = collider(5,1);
+const path4 = collider(7,1);
+const path5 = collider(1,2);
+const product = path1 * path2 * path3 * path4 * path5;
 
-console.log(collisions);
+console.log(`path 1 ${path1}\npath 2 ${path2}\npath 3 ${path3}\npath 4 ${path4}\npath 5 ${path5}
+product ${product}`);
