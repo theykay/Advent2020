205a927b0f59307559b1fb86f2ce4661dbebd500
TheyKay <murraykyleb@gmail.com>
2020-12-09 21:24:01 -0700

    starting on part 2

--- 8-Handheld-Halting/solution.js
+++ 8-Handheld-Halting/solution.js
@@ -9,12 +9,11 @@ converted.forEach(entry => {
   }
   commands.push(obj);
 })
-console.log(commands);
-
-let accumulator = 0;
-let positions = [];
+// console.log(commands);
 
 const runProgram = () => {
+  let accumulator = 0;
+  let positions = [];
   for (let i = 0; i < commands.length;) {
     console.log(i, positions);
     if (!positions.includes(i)) {
@@ -39,4 +38,42 @@ const runProgram = () => {
   }
 }
 let score = runProgram();
-console.log(score);
+// console.log(score);
+
+const runProgram2 = () => {
+  let accumulator = 0;
+  let positions = [];
+  for (let i = 0; i < commands.length;) {
+    console.log(i, positions);
+    if (!positions.includes(i)) {
+      positions.push(i);
+      switch (commands[i].instruction) {
+        case "acc":
+          accumulator += commands[i].number;
+          i++;
+          break;
+        case "jmp":
+          i += commands[i].number;
+          break;
+        case "nop":
+          i++;
+          break;
+        default:
+          break;
+      }
+    } else if (positions.includes(i)) {
+      return accumulator;
+    }
+  }
+  return accumulator;
+}
+
+const part2 = () => {
+  let accumulator = 0;
+  let rules = [];
+  for (let i = 0; i < commands.length;) {
+    i
+  }
+}
+
+console.log(runProgram2());
